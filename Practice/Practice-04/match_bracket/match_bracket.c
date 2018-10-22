30729610a8f097224fa663eb4459a9163dcfb217
YeLing0119 <yeling0119@163.com>
2018-10-22 13:27:43 +0800

    Changes to be committed: 	new file:   match_bracket/match_bracket 	new file:   match_bracket/match_bracket.c
     Changes not staged for commit:
    	modified:   ../../C_Practice/base-struct-data/List/double_link/tips.md
    	modified:   ../Practice-01/tips.txt
    	modified:   ../../README.md
    
     Untracked files:
    	../../Linux/Dict/
    	../../test/.abc
    	../../test/abc

---
+++ Practice/Practice-04/match_bracket/match_bracket.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int stack_point = 0;
+char stack_bracket[100];
+
+void push(char buff)
+{
+	stack_bracket[stack_point] = buff;
+	stack_point++;
+}
+
+void pop()
+{
+	stack_point--;
+}
+
+void cheak()
+{
+	if (stack_bracket[stack_point - 1] == '}'||stack_bracket[stack_point - 2] == '{'){
+		pop(); pop();
+	}
+}
+
+int main(){
+	char code[10240] = { 0 };
+	int count = 0;	//记录现在一共有多少个字符
+	printf("**************************************\n");
+	printf("*****  欢迎使用括号匹配检查系统  *****\n");
+	printf("**************************************\n");
+	printf("输入你要检测的代码(按ctrl + z 结束输入):\n");
+	while ((code[count]=getchar())!=EOF){
+		count++;
+	}
+	//检测括号匹配
+	for (int i = 0; i < count; i++)
+	{
+		if (code[i] == '{'){
+			push('{'); 
+		}
+		if (code[i] == '}'){
+			push('}');
+			cheak();
+		}
+	}
+
+	if (stack_point == 0){
+		printf("匹配成功\n");
+	}else{
+		printf("匹配失败\n");
+	}
+}
